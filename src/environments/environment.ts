ab76c7815da6871c120028a48927809f989ca582
DiegoDominguez <ddominguez@stx.com.mx>
2021-02-10 23:25:39 -0600

    Inicializacion projecto angular e instalacion AngularFire

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA6mZq5R-1Yvpno8wgIGbHWq77xCGMAT5M',
+    authDomain: 'firechat-708c8.firebaseapp.com',
+    databaseURL: 'https://firechat-708c8.firebaseio.com',
+    projectId: 'firechat-708c8',
+    storageBucket: 'firechat-708c8.appspot.com',
+    messagingSenderId: '299065763211',
+    appId: '1:299065763211:web:c2218e346c377ce676bb8e',
+    measurementId: 'G-NPZ19R0CSX'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
